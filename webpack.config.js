61e27f3c57c810f17a06e6c1fd79e5e095826405
caspa2289 <65313374+caspa2289@users.noreply.github.com>
2024-02-03 23:16:50 +0300

    feat: webpack boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    entry: {
+        index: './src/index.ts'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ title: 'Webby' }),
+    ],
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+}
